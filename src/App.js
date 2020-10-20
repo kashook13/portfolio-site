746f3cb7a8e25eaa51597b23d494c544cd941cc1
ZachJHall <zachjohnhall@gmail>
2020-10-20 15:10:39 -0500

    Updated changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Contact from './components/contact';
+import "./css/index.css";
+import * as Info from './info';
+import MainPhoto from './images/mainphoto.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="index-content">
+
+      <div class="short-biography">
+        <img id = "user-image" src= {MainPhoto} />
+        <h1>I'm {Info.FullName}</h1>
+        <h2>A {Info.JobTitle} at <a href={Info.CompanyWebsite}>{Info.CompanyName}</a></h2>
+      </div>
+
+      <Contact />
+
     </div>
   );
 }
